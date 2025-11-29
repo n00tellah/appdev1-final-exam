4931c778805f045e8f0e1126f8087a11b121179f
Your Estela <jalacestelamae@gmail.com>
2025-11-29 10:58:06 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/appdev1-final-exam/",   // must match your repo name
+});
